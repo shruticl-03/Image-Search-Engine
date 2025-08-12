6658bcb4fd8d4cb05a44a4dbe6e7cdaa198ce042
Shruthi L <Shruthi@agpl.net>
2025-08-12 10:56:54 +0530

    In script.js used API by unsplash website and implemeted

---
+++ script.js
@@ -0,0 +1,14 @@
+const accessKey = "JHNZimHxATkg8JQE8GC8xHhqgEOOXbJV2liQAZ7N2Fw";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+}
