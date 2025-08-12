a18fc96355eab7a8e1824986378d74e7911a3b4a
Shruthi L <Shruthi@agpl.net>
2025-08-12 11:12:41 +0530

    Inserted Response Function

--- script.js
+++ script.js
@@ -11,4 +11,14 @@ let page = 1;
 async function searchImages() {
   keyword = searchBox.value;
   const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
 }
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
