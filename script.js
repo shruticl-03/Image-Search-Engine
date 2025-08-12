a1d28c183c4bf6864a8401a417dc885500d79b6d
Shruthi L <Shruthi@agpl.net>
2025-08-12 12:36:31 +0530

    The Images is displaying in correct order

--- script.js
+++ script.js
@@ -10,11 +10,24 @@ let page = 1;
 
 async function searchImages() {
   keyword = searchBox.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  // console.log(data); //displaying in console
+
+  const results = data.results;
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
+  
 }
 
 searchForm.addEventListener("submit", (e) => {
